3cee19ad2c3199f6aacb17eb9f0d1868c216393b
Martin Totev <martintotev89@gmail.com>
2019-12-03 22:32:34 +0900

    Section 1 and 2
    Getting started with iOS13 and Swift and getting comfortable with the Xcode environment.

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Martin Totev on 2019/12/03.
+//  Copyright © 2019 Martin Totev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Martin Totev on 2019/12/01.
+//  Copyright © 2019 Martin Totev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
